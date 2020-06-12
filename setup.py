206bb778bd7a2ff7a40336734f7935c6858665b9
Evan Quinlan <evan.quinlan@gmail.com>
2020-06-12 13:36:58 -0400

    Include data file in package

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyhavamal", # Replace with your own username
-    version="0.1.0",
+    version="0.1.1",
     author="Evan Quinlan",
     author_email="evan.quinlan@gmail.com",
     description="A CSV file containing the Old Norse Hávamál stanzas and a simple Python app for accessing them.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/equinlan/pyhavamal",
-    packages=setuptools.find_packages(),
+    packages=['pyhavamal'],
+    package_dir={'pyhavamal': 'pyhavamal'},
+    package_data={'pyhavamal': ['data/stanzas']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.7'
 )
